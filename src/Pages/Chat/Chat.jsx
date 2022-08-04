cefbf1c53be49a85a0a73559d219bddb3ada68a7
Aswin Asok <aswinasokofficial@gmail.com>
2022-08-04 09:22:41 +0530

    styling

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 import {
   collection,
   onSnapshot,
   query,
   orderBy,
   addDoc,
-  Timestamp
+  Timestamp,
 } from "firebase/firestore";
 
 import styles from "./Chat.module.css";
@@ -13,19 +14,20 @@ import styles from "./Chat.module.css";
 const Chat = ({ auth, db, setMessages, messages }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [message, setMessage] = useState("");
-  const sentMessage = async() => {
+
+  const sentMessage = async () => {
     const docRef = await addDoc(collection(db, "messages"), {
       text: message,
-      timestamp: Timestamp.now()
+      timestamp: Timestamp.now(),
     });
     console.log("Document written with ID: ", docRef.id);
-  }
+  };
+
   useEffect(() => {
     console.log(auth.currentUser);
     setIsLoading(true);
-    const q = query(collection(db, "messages"), orderBy("timestamp", "desc"));
     onSnapshot(
-      q,
+      query(collection(db, "messages"), orderBy("timestamp", "asc")),
       (snapshot) => {
         setMessages([]);
         snapshot.docs.map(function (doc) {
@@ -38,38 +40,42 @@ const Chat = ({ auth, db, setMessages, messages }) => {
     );
     setIsLoading(false);
   }, []);
-  console.log(messages);
+
   return (
     <>
       {isLoading && <p>Loading</p>}
       {!isLoading && (
         <div className={styles.chatmain_container}>
-          <div className={styles.chat_container}>
-            <div className={styles.chat_view}>
-              {messages.map((message) => (
-                <p>{message}</p>
-              ))}
+          <div className={styles.chat_main}>
+            <ScrollToBottom className={styles.chat_container}>
+              
+                {messages.map((message) => (
+                  <div className={styles.message_box}>
+                    <p className={styles.message_text}>{message}</p>
+                  </div>
+                ))}
+              
+            </ScrollToBottom>
+            <div className={styles.inputmain_container}>
+              <input
+                type="text"
+                name="input"
+                id="input"
+                placeholder="Enter Message Here!"
+                className={styles.input_field}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  sentMessage();
+                }}
+                className={styles.sent_btn}
+              >
+                Sent
+              </button>
             </div>
-          </div>
-          <div className={styles.inputmain_container}>
-            <input
-              type="text"
-              name="input"
-              id="input"
-              placeholder="Enter Message Here!"
-              className={styles.input_field}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-            <button
-              onClick={() => {
-                sentMessage();
-              }}
-              className={styles.sent_btn}
-            >
-              Sent
-            </button>
           </div>
         </div>
       )}
